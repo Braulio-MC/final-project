6e2f132ba4607194f337634785b9e771783682d5
Braulio Martinez Carrillo <braulio.mart.carr@gmail.com>
2024-05-21 22:00:43 -0600

    firebase functions added to support algolia indices sync

--- src/core/AlgoliaHelper.ts
+++ src/core/AlgoliaHelper.ts
@@ -1,35 +1,35 @@
 import algoliasearch from 'algoliasearch/lite'
 import { algoliaConfig } from './Configuration'
-import functions from 'firebase-functions'
+import { firestore } from 'firebase-functions'
 
 export const algoliaClient = algoliasearch(algoliaConfig.applicationId as string, algoliaConfig.apiKey as string)
 export const storeAlgoliaIndex = algoliaClient.initIndex(algoliaConfig.storeIndex as string)
 export const productAlgoliaIndex = algoliaClient.initIndex(algoliaConfig.productIndex as string)
 
-export const updateProductInIndex = functions.firestore
-  .document('products/{productId}')
+export const updateProductInIndex = firestore
+  .document('/products/{productId}')
   .onUpdate(change => {
     const newData = change.after.data()
     newData.objectID = change.after.id
     return (productAlgoliaIndex as any).saveObject(newData)
   })
 
-export const deleteProductInIndex = functions.firestore
-  .document('products/{productId}')
+export const deleteProductInIndex = firestore
+  .document('/products/{productId}')
   .onDelete(snapshot => {
     (productAlgoliaIndex as any).deleteObject(snapshot.id)
   })
 
-export const updateStoreInIndex = functions.firestore
-  .document('stores/{storeId}')
+export const updateStoreInIndex = firestore
+  .document('/stores/{storeId}')
   .onUpdate(change => {
     const newData = change.after.data()
     newData.objectID = change.after.id
     return (storeAlgoliaIndex as any).saveObject(newData)
   })
 
-export const deleteStoreInIndex = functions.firestore
-  .document('stores/{storeId}')
+export const deleteStoreInIndex = firestore
+  .document('/stores/{storeId}')
   .onDelete(snapshot => {
     (storeAlgoliaIndex as any).deleteObject(snapshot.id)
   })
