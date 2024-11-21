2b85242e3eaa6e0a2585dba20d0f42582b38d901
Braulio Martinez Carrillo <braulio.mart.carr@gmail.com>
2024-11-20 18:57:43 -0600

    location functions removed

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { uploadImage as uplIFunc } from './functions/UploadImage'
 import { updateImage as updIFunc } from './functions/UpdateImage'
 import * as categoryFuncs from './functions/Category.functions'
 import * as discountFuncs from './functions/Discount.functions'
-import * as locationFuncs from './functions/Location.functions'
 import * as productFuncs from './functions/Product.functions'
 import * as pushNotificationFuncs from './functions/PushNotification.functions'
 import * as messagingFuncs from './functions/Messaging.functions'
@@ -15,7 +14,6 @@ export const uploadImage = uplIFunc
 export const updateImage = updIFunc
 export const category = categoryFuncs
 export const discount = discountFuncs
-export const location = locationFuncs
 export const product = productFuncs
 export const pushNotification = pushNotificationFuncs
 export const messaging = messagingFuncs
